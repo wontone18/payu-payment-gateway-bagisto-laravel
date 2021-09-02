270d0097b46cc1547720ba9333affe19fa7dbdb1
Saju Gopal Ambatt <saju.g@wontonetech.com>
2021-09-02 15:18:35 +0530

    Controller issue fixed
    Controller issue fixed

--- src/Http/Controllers/PayuController.php
+++ src/Http/Controllers/PayuController.php
@@ -5,6 +5,7 @@ namespace Wontonee\Payu\Http\Controllers;
 
 use Webkul\Checkout\Facades\Cart;
 use Webkul\Sales\Repositories\OrderRepository;
+use Webkul\Sales\Repositories\InvoiceRepository;
 use Illuminate\Support\Facades\Config;
 
 class PayuController extends Controller
@@ -16,6 +17,12 @@ class PayuController extends Controller
 	 * @var \Webkul\Sales\Repositories\OrderRepository
 	 */
 	protected $orderRepository;
+	/**
+	 * InvoiceRepository $invoiceRepository
+	 *
+	 * @var \Webkul\Sales\Repositories\InvoiceRepository
+	 */
+	protected $invoiceRepository;
 
 	/**
 	 * Create a new controller instance.
@@ -23,9 +30,10 @@ class PayuController extends Controller
 	 * 
 	 * @return void
 	 */
-	public function __construct(OrderRepository $orderRepository)
+	public function __construct(OrderRepository $orderRepository,  InvoiceRepository $invoiceRepository)
 	{
 		$this->orderRepository = $orderRepository;
+		$this->invoiceRepository = $invoiceRepository;
 	}
 
 	/**
@@ -38,16 +46,16 @@ class PayuController extends Controller
 	{
 		$cart = Cart::getCart();
 
-		
+
 		$billingAddress = $cart->billing_address;
 
 		$MERCHANT_KEY = core()->getConfigData('sales.paymentmethods.payu.payu_merchant_key');
 		$SALT = core()->getConfigData('sales.paymentmethods.payu.salt_key');
 
-	
-		if(core()->getConfigData('sales.paymentmethods.payu.payu-website') == "Sandbox"):
-		 	$PAYU_BASE_URL = "https://sandboxsecure.payu.in";        // For Sandbox Mode
-		else:
+
+		if (core()->getConfigData('sales.paymentmethods.payu.payu-website') == "Sandbox") :
+			$PAYU_BASE_URL = "https://sandboxsecure.payu.in";        // For Sandbox Mode
+		else :
 			$PAYU_BASE_URL = "https://secure.payu.in";        // For Sandbox Mode
 		endif;
 
@@ -66,7 +74,7 @@ class PayuController extends Controller
 			"furl" => route('payu.failure'),
 			"curl" => "cancel",
 			"hash" => '',
-			"productinfo" => 'Bagisto Order no '.$cart->id
+			"productinfo" => 'Bagisto Order no ' . $cart->id
 		);
 		/*** has  */
 		// Hash Sequence
@@ -82,7 +90,7 @@ class PayuController extends Controller
 		$hash_string .= $SALT;
 		$hash = strtolower(hash('sha512', $hash_string));
 		$action = $PAYU_BASE_URL . '/_payment';
-		return view('payu::payu-redirect')->with(compact('MERCHANT_KEY','SALT','action','posted','hash'));
+		return view('payu::payu-redirect')->with(compact('MERCHANT_KEY', 'SALT', 'action', 'posted', 'hash'));
 	}
 
 	/**
@@ -90,9 +98,15 @@ class PayuController extends Controller
 	 */
 	public function success()
 	{
+
 		$order = $this->orderRepository->create(Cart::prepareDataForOrder());
+		$this->orderRepository->update(['status' => 'processing'], $order->id);
+		if ($order->canInvoice()) {
+			$this->invoiceRepository->create($this->prepareInvoiceData($order));
+		}
 		Cart::deActivateCart();
 		session()->flash('order', $order);
+		// Order and prepare invoice
 		return redirect()->route('shop.checkout.success');
 	}
 
@@ -103,6 +117,21 @@ class PayuController extends Controller
 	{
 		session()->flash('error', 'Payu payment either cancelled or transaction failure.');
 		return redirect()->route('shop.checkout.cart.index');
+	}
 
+	/**
+	 * Prepares order's invoice data for creation.
+	 *
+	 * @return array
+	 */
+	protected function prepareInvoiceData($order)
+	{
+		$invoiceData = ["order_id" => $order->id,];
+
+		foreach ($order->items as $item) {
+			$invoiceData['invoice']['items'][$item->id] = $item->qty_to_invoice;
+		}
+
+		return $invoiceData;
 	}
 }
